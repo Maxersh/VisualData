3340a2e98aa40bceb7cc03e716303bd41041f703
m.krokhin <maks.krohing@gmail.com>
2020-12-30 16:43:04 +0300

    Add some feature for random walk.

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import matplotlib.pyplot as plt
 import pygal
+from random_walk import RandomWalk
 
-x_values = list(range(1,1001))
-y_values = [ x**2 for x in x_values ]
+point_numbers = list(range(30))
 
-plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues,
-            edgecolor='none', s=100)
-plt.savefig('squares_plot.png', bbox_inches='tight')
+walk = RandomWalk(len(point_numbers))
+for i in range(1):
+    walk.fill()
+    walk.paint()
+    #print(walk.colors)
+    plt.scatter(walk.x_values, walk.y_values, c=walk.colors,
+                cmap=plt.cm.Blues, edgecolor='none', s=6)
+    #plt.scatter(0, 0, c='green', edgecolors='none', s=80)
+    #plt.scatter(walk.x_values[-1], walk.y_values[-1], c='red',
+    #            edgecolors='none', s=80)
+    walk.clear()
+plt.savefig('squares_plot_new.png', bbox_inches='tight')
+#plt.savefig('squares_plot.png', bbox_inches='tight')
 #plt.show()
--- random_walk.py
+++ random_walk.py
@@ -1,5 +1,5 @@
 from random import choice
-
+from time import time
 class RandomWalk():
 
     def __init__(self, num_points=500):
@@ -7,6 +7,9 @@ class RandomWalk():
 
         self.x_values = [0]
         self.y_values = [0]
+
+        self.colors = []
+
 
     def fill(self):
 
@@ -23,6 +26,22 @@ class RandomWalk():
             self.x_values.append(x_new_val)
             self.y_values.append(y_new_val)
 
+    def paint(self):
+
+        gap = 4
+        points = zip(self.x_values, self.y_values)
+        for point in (self.x_values, self.y_values):
+            number = 0
+            print(point)
+            for pnt in (self.x_values, self.y_values):
+                print(pnt)
+                number += int(abs(pnt[0] - point[0]) <= gap and abs(pnt[1] - point[1]) <= gap)
+            print('!!{}'.format(number))
+            number -= 1
+            self.colors.append(number)
+        print(self.colors)
+
     def clear(self):
         self.x_values = [0]
         self.y_values = [0]
+        self.colors = []
